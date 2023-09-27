d4aef293ce28f3bf8f5e5d01ad5c36f1ae7b7065
acolombo09 <acolombo0911@gmail.com>
2023-09-27 19:37:02 +0200

    migrations_phase-completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,74 @@
             </div>
           </div>
           @endforeach --}}
+        <table class="table">
+
+          <thead>
+  
+            <tr>
+
+                <th scope="col">#</th>
+
+          
+                    
+                <th scope="col">
+                </th>
+
+        
+            </tr>
+  
+          </thead>
+  
+          <tbody>
+
+            @foreach ($trains as $train)
+            <tr>
+
+                <th scope="row">
+                    {{$train->id}}
+                </th>                    
+              
+                <td>
+                  {{$train->company}}
+                </td>
+                
+                <td>
+                  {{$train->departure_station}}
+                </td>
+                
+                <td>
+                  {{$train->arrival_station}}
+                </td>
+                
+                <td>
+                  {{$train->departure_time}}
+                </td>
+                
+                <td>
+                  {{$train->arrival_time}}
+                </td>
+                
+                <td>
+                  {{$train->train_code}}
+                </td>
+                
+                <td>
+                  {{$train->cart_number}}
+                </td>
+                
+                <td>
+                  {{$train->in_time}}
+                </td>
+                
+                <td>
+                  {{$train->canceled}}
+                </td>
+                
+            </tr>
+            @endforeach                                               
+          </tbody>
+  
+        </table>
       </div>
     </div>
   </div>
